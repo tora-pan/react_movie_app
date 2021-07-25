db831ba1a3aa6fdb3353ba874855dcd887202b84
tora_pan <travis.pandos@gmail.com>
2021-07-24 21:59:49 -0700

    comment out images_api

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const IMAGES_API = "https://image.tmdb.org/t/p/w1280";
+// const IMAGES_API = "https://image.tmdb.org/t/p/w1280";
 
 const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
